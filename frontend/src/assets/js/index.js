1af76e73408885339115dce65f7b0450e4474f23
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2024-09-25 14:55:52 -0300

    Refatorar autoplay do vídeo de fundo para desabilitar o som e adicionar controle de volume

--- frontend/src/assets/js/index.js
+++ frontend/src/assets/js/index.js
@@ -1,9 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    //declaração de variáveis
-    const audio = document.getElementById('audio-play');
-    audio.autoplay = true;
-    console.log(audio);
-    audio.volume = 0.1;
 
     const questions = [{
         question: "Qual das seguintes opções NÃO é uma linguagem de programação?",
@@ -53,12 +48,12 @@ document.addEventListener('DOMContentLoaded', function () {
     ]
 
     //declaração com o query
-    const answerText = document.querySelectorAll('.answer-text');
-    const buttons = document.querySelectorAll('.answer');
-    const totalCurrentQuestion = document.querySelector('.totalCurrentQuestion')
-    const questionText = document.querySelector('.question-text');
-    const currentlyQuestion = document.querySelector('#currentQuestion');
-    const currentlyQuestionIndex = 0;
+    let answerText = document.querySelectorAll('.answer-text');
+    let buttons = document.querySelectorAll('.answer');
+    let totalCurrentQuestion = document.querySelector('.totalCurrentQuestion')
+    let questionText = document.querySelector('.question-text');
+    let currentlyQuestion = document.querySelector('#currentQuestion');
+    let currentlyQuestionIndex = 0;
 
     function showQuestion() {
 
@@ -109,8 +104,5 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log(`As perguntas são: ${questions}`);
     // console da pergunta atual
     console.log(`A pergunta atual é: ${currentlyQuestionIndex}`);
-    // console do acerto da resposta
-    console.log(`A resposta correta é: ${correct}`);
-    // console se errou a resposta
-    console.log(`Você errou a resposta! ${}`);
+    console.log(`A pergunta ${currentlyQuestionIndex} é: ${questions[currentlyQuestionIndex]}`);
 });
